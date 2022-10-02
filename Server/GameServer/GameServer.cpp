f6c4e8def292dab2c0f1a91a9368713cee1bc4c6
MecaDragon <kvg753@gmail.com>
2022-10-02 20:02:07 +0900

    메모리 모델

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -6,46 +6,50 @@
 #include <mutex>
 #include <Windows.h>
 
-atomic<bool> flag;
+atomic<bool> ready;
+int32 value;
+
+void Producer()
+{
+    value = 10;
+
+    ready.store(true, memory_order::memory_order_relaxed);
+}
+
+void Consumer()
+{
+    while (ready.load(memory_order::memory_order_relaxed) == false)
+        ;
+
+    cout << value << endl;
+}
 
 int main()
 {
-    flag = false;
+    ready = false;
+    value = 0;
+    thread t1(Producer);
+    thread t2(Consumer);
+    t1.join();
+    t2.join();
 
-    // flag = true
-    flag.store(true);
+    /* Memory Model(정책)
+    1) Sequentially Consistent (seq_cst)
+    2) Acquire-Release (consume, acquire, release, acq_rel)
+    3) Relaxed (relaxed)
 
-    //bool val = flag
-    bool val = flag.load();
+    1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
+       가시성 문제 해결! 코드 재배치 바로 해결!
 
-    // 이전 flag 값을 prev에 넣고 flag 값을 수정
-    {
-        bool prev = flag.exchange(true);
-        //bool prev =flag;
-        //flag = true;
-    }
+    2) acquire-release
+       딱 중간
+       release 명령 이전의 메모리 명령들이 해당 명령 이후로 재배치 되는 것을 금지
+       그리고 acquire로 같은 변수를 읽는 스레드가 있다면
+       release 이전의 명령들이 -> acquire 하는 순간에 관찰 가능 (가시성 보장)
 
-    // CAS (Compare-And_Swap) 조건부 수정
-    {
-        bool expected = false;
-        bool desired = true;
-        flag.compare_exchange_strong(expected, desired);
-        /*
-        if (flag == expected)
-        {
-            flag = desired;
-            return true;
-        }
-        else
-        {
-            expected = flag;
-            return false;
-        }
-        */
-
-        bool expected = false;
-        bool desired = true;
-        // strong이랑 기본적으로 똑같지만 중간에 다른 스레드의 인터럽트를 받아서 중간에 실패할 수 있음
-        flag.compare_exchange_weak(expected, desired);
-    }
+    3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
+       너무나도 자유롭다
+       코드 재배치도 멋대로 가능! 가시성 해결 NO!
+       가장 기본 조건 (동일 객체에 대한 동일 관전 순서만 보장)
+    */
 }
