02b6263d208bbb2388c07378a69f6287354a0d1e
MecaDragon <kvg753@gmail.com>
2022-10-02 20:09:11 +0900

    TLS 실습

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -6,50 +6,31 @@
 #include <mutex>
 #include <Windows.h>
 
-atomic<bool> ready;
-int32 value;
+thread_local int32 LThread = 0;
 
-void Producer()
+void ThreadMain(int32 threadid)
 {
-    value = 10;
+    LThread = threadid;
 
-    ready.store(true, memory_order::memory_order_relaxed);
-}
-
-void Consumer()
-{
-    while (ready.load(memory_order::memory_order_relaxed) == false)
-        ;
-
-    cout << value << endl;
+    while (true)
+    {
+        cout << "Hi I am Thread " << LThread << endl;
+        this_thread::sleep_for(1s);
+    }
 }
 
 int main()
 {
-    ready = false;
-    value = 0;
-    thread t1(Producer);
-    thread t2(Consumer);
-    t1.join();
-    t2.join();
+    vector<thread> threads;
 
-    /* Memory Model(정책)
-    1) Sequentially Consistent (seq_cst)
-    2) Acquire-Release (consume, acquire, release, acq_rel)
-    3) Relaxed (relaxed)
+    for (int32 i = 0; i < 10; i++)
+    {
+        int32 threadId = i + 1;
+        threads.push_back(thread(ThreadMain, threadId));
+    }
 
-    1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
-       가시성 문제 해결! 코드 재배치 바로 해결!
-
-    2) acquire-release
-       딱 중간
-       release 명령 이전의 메모리 명령들이 해당 명령 이후로 재배치 되는 것을 금지
-       그리고 acquire로 같은 변수를 읽는 스레드가 있다면
-       release 이전의 명령들이 -> acquire 하는 순간에 관찰 가능 (가시성 보장)
-
-    3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
-       너무나도 자유롭다
-       코드 재배치도 멋대로 가능! 가시성 해결 NO!
-       가장 기본 조건 (동일 객체에 대한 동일 관전 순서만 보장)
-    */
+    for (thread& t : threads)
+    {
+        t.join();
+    }
 }
