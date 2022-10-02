7a63a6cbabdd716f4f4aa833f546f6a3abeddb7a
MecaDragon <kvg753@gmail.com>
2022-10-02 16:31:04 +0900

    스핀락 구현

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,8 +1,63 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+class SpinLock
+{
+public:
+    void lock()
+    {
+        // CAS (Compare-And-Swap)
+        bool expected = false;
+        bool desired = true;
+
+        while(_locked.compare_exchange_strong(expected, desired) == false)
+        {
+            expected = false;
+        }
+    }
+
+    void unlock()
+    {
+        _locked.store(false);
+    }
+
+private:
+    atomic<bool> _locked = false;
+};
+
+int32 sum = 0;
+mutex m;
+SpinLock spinLock;
+
+void Add()
+{
+    for (int32 i = 0; i < 10'0000; i++)
+    {
+        lock_guard<SpinLock> guard(spinLock);
+        sum++;
+    }
+}
+
+void Sub()
+{
+    for (int32 i = 0; i < 10'0000; i++)
+    {
+        lock_guard<SpinLock> guard(spinLock);
+        sum--;
+    }
+}
 
 int main()
 {
-    HelloWorld();
+    thread t1(Add);
+    thread t2(Sub);
+
+    t1.join();
+    t2.join();
+
+    cout << sum << endl;
 }
