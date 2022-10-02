5886297ba24fa75a0c9cd48d4e3715356ab5941c
MecaDragon <kvg753@gmail.com>
2022-10-03 00:50:40 +0900

    mutex 락 말고 직접 쓸 락을 구현 define을 이용해 쓰기 편하게 함

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,14 +7,58 @@
 #include <Windows.h>
 #include "ThreadManager.h"
 
-CoreGlobal Core;
+class TestLock
+{
+	USE_LOCK;
 
-void ThreadMain()
+public:
+	int32 TestRead()
+	{
+		READ_LOCK;
+		if (_queue.empty())
+			return -1;
+
+		return _queue.front();
+	}
+
+	void TestPush()
+	{
+		WRITE_LOCK;
+
+		_queue.push(rand() % 100);
+	}
+
+	void TestPop()
+	{
+		WRITE_LOCK;
+
+		if (_queue.empty() == false)
+			_queue.pop();
+	}
+
+private:
+	queue<int32> _queue;
+};
+
+TestLock testLock;
+
+void ThreadWrite()
 {
 	while (true)
 	{
-		cout << "Hello Iam thrad.. " << LThreadId << endl;
-		this_thread::sleep_for(1s);
+		testLock.TestPush();
+		this_thread::sleep_for(1ms);
+		testLock.TestPop();
+	}
+}
+
+void ThreadRead()
+{
+	while (true)
+	{
+		int32 value = testLock.TestRead();
+		cout << value << endl;
+		this_thread::sleep_for(1ms);
 	}
 }
 
@@ -22,7 +66,13 @@ int main()
 {
 	for (int32 i = 0; i < 5; i++)
 	{
-		GThreadManager->Launch(ThreadMain);
+		GThreadManager->Launch(ThreadWrite);
 	}
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadRead);
+	}
+
 	GThreadManager->Join();
 }
