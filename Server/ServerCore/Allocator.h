7395ead0c3113d7f45c3056bd3bee6f9f63f1d2b
MecaDragon <kvg753@gmail.com>
2022-10-03 19:11:17 +0900

    Stomp Allocator 메모리 오염을 잡기 위해 직접 가상메모리에 메모리 할당을 해준다!

--- Server/ServerCore/Allocator.h
+++ Server/ServerCore/Allocator.h
@@ -12,3 +12,15 @@ public:
 
 };
 
+/*-----------------
+	StompAllocator
+------------------*/
+
+class StompAllocator
+{
+	enum { PAGE_SIZE = 0x1000 };
+public:
+	static void*	Alloc(int32 size);
+	static void		Release(void* ptr);
+
+};
--- Server/ServerCore/CoreMacro.h
+++ Server/ServerCore/CoreMacro.h
@@ -18,8 +18,8 @@
 -----------------*/
 
 #ifdef _DEBUG
-#define xxalloc(size)	BaseAllocator::Alloc(size)
-#define xrelease(ptr)	BaseAllocator::Release(ptr)
+#define xxalloc(size)	StompAllocator::Alloc(size)
+#define xrelease(ptr)	StompAllocator::Release(ptr)
 #else
 #define xalloc(size)	BaseAllocator::Alloc(size)
 #define xrelease(ptr)	BaseAllocator::Release(ptr)
