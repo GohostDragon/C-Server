a8b67e6fded918e3e8af496bfb9c696e863a1e83
MecaDragon <kvg753@gmail.com>
2022-10-03 17:41:56 +0900

    스마트포인터 shared 포인터에 대해

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,4 +9,13 @@
 
 int main()
 {
+	/*
+	shared_ptr : RefCount 0이 되면 소멸(shared 0), 하지만 서로 참조하는 경우
+	해제를 못하는 경우가 있어 메모리 릭이 생긴다
+	weak_ptr : 서로s 참조 에러를 해결하기 위해 weakCount 숫자를 올리도록 한다
+	shared 포인트가 없어지면 소멸하지만 weakCount가 남아있을 경우
+	RefCountBlock이 존재한다!
+
+	[T*][RefCountBlock(shared, weakCount)]
+	*/
 }
