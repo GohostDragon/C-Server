231d2dfb96b6ba15fc0cb0ec6f86af9192bce90a
MecaDragon <kvg753@gmail.com>
2022-10-29 22:24:16 +0900

    Object Pool

--- Server/ServerCore/Memory.h
+++ Server/ServerCore/Memory.h
@@ -34,7 +34,7 @@ private:
 template<typename Type, typename... Args>
 Type* xnew(Args&&... args)
 {
-	Type* memory = static_cast<Type*>(xxalloc(sizeof(Type)));
+	Type* memory = static_cast<Type*>(PoolAllocator::Alloc(sizeof(Type)));
 	new(memory)Type(forward<Args>(args)...); // placement new (생성자 호출)
 	return memory;
 }
@@ -43,5 +43,11 @@ template<typename Type>
 void xdelete(Type* obj)
 {
 	obj->~Type(); // 소멸자 호출
-	xrelease(obj);
+	PoolAllocator::Release(obj);
+}
+
+template<typename Type>
+shared_ptr<Type> MakeShared()
+{
+	return shared_ptr<Type>{xxnew<Type>(), xdelete<Type>};
 }
